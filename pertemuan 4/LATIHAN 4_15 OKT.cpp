a3d06d4ddb2f1f685110e6b6ddaa311ab8d5a91f
Ditya-bagas <dityabagas15@gmail.com>
2024-10-16 00:12:04 +0800

    menambahkan pertemuan 4

---
+++ pertemuan 4/LATIHAN 4_15 OKT.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void printMessage(){
+    cout << "Hello, world!" << endl;
+}
+int main(){
+    printMessage();
+    return 0;
+}
+//penambahan tanda kurung () pada printMessage fase 2
---
+++ pertemuan 4/LATIHAN 6_15 OKT .cpp
@@ -0,0 +1,13 @@
+#include <ionstream>
+using namespace std;
+
+int main(){
+    int x = 10, y = 20;
+    int z = x + y;
+
+    count << "sum of x and y is " << z << endl;
+
+    return 0;
+}
+//penambahan titik koma ; pada endl
+
